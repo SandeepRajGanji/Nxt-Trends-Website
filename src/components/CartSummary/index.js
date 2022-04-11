b6a03e1ea2b05fbcfbae6e05c6b833fdc6bfc011
SandeepRajGanji <deepuganji@gmail.com>
2022-04-11 11:05:44 +0000

    Cart Features is updated

---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,32 @@
+// Write your code here
+import CartContext from '../../context/CartContext'
+import './index.css'
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const totalAmountArray = cartList.map(
+        eachItem => eachItem.price * eachItem.quantity,
+      )
+      const totalAmount = totalAmountArray.reduce(
+        (sum, eachItem) => sum + eachItem,
+      )
+      return (
+        <div className="billing-container">
+          <div className="billing-content">
+            <div className="order-summary">
+              <h1 className="order-total-heading">Order Total</h1>
+              <h1 className="total-amount">Rs {totalAmount} /-</h1>
+            </div>
+            <p className="items-in-cart">{cartList.length} Items in cart</p>
+            <button type="button" className="checkout">
+              Checkout
+            </button>
+          </div>
+        </div>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default CartSummary
